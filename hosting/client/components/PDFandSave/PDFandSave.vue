7715e683213e1828bfcf8aa940645b1d9daae4ae
mdunisch <subseven7>
2019-02-11 11:43:56 +0100

    add saving

--- hosting/client/components/PDFandSave/PDFandSave.vue
+++ hosting/client/components/PDFandSave/PDFandSave.vue
@@ -4,13 +4,25 @@
             <el-button type="primary" icon="el-icon-download">Download als PDF</el-button>
         </el-col>
         <el-col :span="12" style="text-align: right;">
-            <el-button type="primary" icon="el-icon-document">Speichern</el-button>
+            <el-button type="primary" icon="el-icon-document" @click="saveState">Speichern</el-button>
         </el-col>
     </el-row>
 </template>
 
 <script>
-  export default {}
+    
+  import { saveAs } from 'file-saver';
+
+  export default {
+
+      methods: {
+          saveState() {
+              const blob = new Blob([JSON.stringify(this.$store.state)], {type: "application/json"});
+              saveAs(blob);
+          }
+      }
+
+  }
 </script>
 
 <style scoped>
